cc151c3d0a58bcc10d9697080f445eae377c6b1f
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-09-21 16:47:47 -0400

    Add project files.

---
+++ IndexersExamples/MusicGenres.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersExamples
+{
+    public class MusicGenres
+    {
+        private string[] genres = new string[5];
+
+        public string this[int index]
+        {
+            get { return genres[index]; }
+            set { genres[index] = value; }
+        }
+    }
+}
---
+++ IndexersExamples/Program.cs
@@ -0,0 +1,15 @@
+﻿using IndexersExamples;
+
+MusicGenres genres = new MusicGenres();
+
+int count = 0;
+while (count < 5)
+{
+    genres[count] = Console.ReadLine();
+    count++;
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Following: " + genres[i]);
+}
